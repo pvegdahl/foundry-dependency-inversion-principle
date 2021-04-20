c29e20d47f465bcebfed8f166c88973fda931dd4
pvegdahl <pvegdahl@gmail.com>
2021-04-20 14:10:22 -0400

    Refactor and break a test before I write it.

--- tests/test_file_sync.py
+++ tests/test_file_sync.py
@@ -6,26 +6,27 @@ from file_sync import file_sync
 
 
 class TestFileSync(unittest.TestCase):
+    def setUp(self) -> None:
+        self.source_dir_name = "source_dir"
+        self.target_dir_name = "target_dir"
+
+        os.mkdir(self.source_dir_name)
+        os.mkdir(self.target_dir_name)
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.source_dir_name)
+        shutil.rmtree(self.target_dir_name)
+
     def test_copy_one_file(self):
-        source_dir_name = "source_dir"
-        target_dir_name = "target_dir"
-
-        os.mkdir(source_dir_name)
-        os.mkdir(target_dir_name)
-
         file_name = "file_1.txt"
         file_contents = "file contents"
-        with open(f"{source_dir_name}/{file_name}", "w") as source_file:
+        with open(f"{self.source_dir_name}/{file_name}", "w") as source_file:
             source_file.write(file_contents)
 
-        try:
-            file_sync(source_dir=source_dir_name, target_dir=target_dir_name)
-            self.assertTrue(os.path.isfile(f"{target_dir_name}/{file_name}"))
+        file_sync(source_dir=self.source_dir_name, target_dir=self.target_dir_name)
+        self.assertTrue(os.path.isfile(os.path.join(self.target_dir_name, file_name)))
 
-            with open(f"{target_dir_name}/{file_name}", "r") as target_file:
-                target_file_contents = target_file.read()
-                self.assertEqual(file_contents, target_file_contents)
-        finally:
-            shutil.rmtree(source_dir_name)
-            shutil.rmtree(target_dir_name)
+        with open(os.path.join(self.target_dir_name, file_name), "r") as target_file:
+            target_file_contents = target_file.read()
+            self.assertEqual(file_contents, target_file_contents)
 
